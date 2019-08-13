5ed22ef70d54e0e02fee1d4671e3aee3f72367d3
Mavireck <mavireck@outlook.fr>
2019-08-13 22:29:30 +0200

    Fixed launcher

--- startLauncher.sh
+++ startLauncher.sh
@@ -6,10 +6,18 @@ cd "${0%/*}" || exit 1
 
 
 # Siphon a few things from nickel's environment
-eval "$(xargs -n 1 -0 <"/proc/$(pidof nickel)/environ" | grep -e DBUS_SESSION_BUS_ADDRESS -e NICKEL_HOME -e WIFI_MODULE -e LANG -e WIFI_MODULE_PATH -e INTERFACE 2>/dev/null)"
-export DBUS_SESSION_BUS_ADDRESS NICKEL_HOME WIFI_MODULE LANG WIFI_MODULE_PATH INTERFACE
+eval "$(xargs -n 1 -0 <"/proc/$(pidof nickel)/environ" | grep -e DBUS_SESSION_BUS_ADDRESS -e DBUS_SESSION -e NICKEL_HOME -e WIFI_MODULE -e LANG -e WIFI_MODULE_PATH -e INTERFACE 2>/dev/null)"
+export DBUS_SESSION_BUS_ADDRESS DBUS_SESSION NICKEL_HOME WIFI_MODULE LANG WIFI_MODULE_PATH INTERFACE
+
 # Flush the disks: might help avoid damaging nickel's DB...
 sync
+
+# For Plato:
+export PLATFORM="mx50-ntx"
+MODEL_NUMBER=$(cut -f 6 -d ',' /mnt/onboard/.kobo/version | sed -e 's/^[0-]*//')
+export MODEL_NUMBER
+export LD_LIBRARY_PATH="libs:${LD_LIBRARY_PATH}"
+
 # Stop kobo software because it's running
 killall nickel hindenburg sickel fickel fmon > /dev/null 2>&1
 
