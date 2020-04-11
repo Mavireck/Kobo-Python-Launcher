21d644b3d323d308030b9ea1522133be2a8d4391
Mavireck <mavireck@outlook.fr>
2020-04-11 16:52:36 +0200

    Updated launcher to the new version of PSSM

--- launcher.py
+++ launcher.py
@@ -4,15 +4,17 @@ import os
 # Load the wrapper module, it's linked against FBInk, so the dynamic loader will take care of pulling in the actual FBInk library
 from _fbink import ffi, lib as FBInk
 import json
+from copy import deepcopy
 # Load Pillow
 from PIL import Image, ImageDraw, ImageFont
 # My own librairies (Kobo-Input-Python, Kobo-Python-OSKandUtils)
 sys.path.append('../Kobo-Input-Python')
 sys.path.append('../Kobo-Python-OSKandUtils')
-sys.path.append('../Kobo-Screen-Stack-Manager')
+sys.path.append('/mnt/onboard/.adds/mavireck/Python-Screen-Stack-Manager')
 import KIP
 import pssm
 import pssmObjectsLibrairy as POL
+import pssm_kobo
 import osk
 import threading
 
@@ -31,23 +33,17 @@ title_font_bold = ImageFont.truetype("fonts/Merriweather-Bold.ttf", 30)
 current_page=0
 apps_per_page = 8
 
+border = 10
+big_border = 3*border
+
 with open('launcher.json') as json_file:
 	appsData = json.load(json_file)
 
 
-def printLauncher(page=current_page):
+def printLauncher(page=0):
+	global current_page
 	current_page = page
-	border = 10
-	big_border = 3*border
 	## Building the menu
-	# Main title
-	rectWidth=int(5*screenWidth/6)
-	titleRectHeight=int(screenHeight/10)
-	rectHeight = int(screenHeight/(apps_per_page+7))
-	rect_x = int(screenWidth/12)
-	titleObj = POL.roundedRectangle(rect_x,big_border,rectWidth,titleRectHeight,fill=rect_color,outline=rect_outline)
-	titleObj = POL.add_centeredText(titleObj,"Welcome to the PythonLauncher!",title_font_bold)
-	screen.addObj(titleObj)
 	# Apps buttons
 	appObjs = []
 	for i in range(min(apps_per_page,len(appsData["apps"][page*apps_per_page:(page+1)*apps_per_page]))):
@@ -59,8 +55,11 @@ def printLauncher(page=current_page):
 		appObj = POL.rectangle(app_x,app_y,app_w,app_h,fill=white,outline=rect_outline)
 		appObj = POL.add_centeredText(appObj,app["title"],small_font)
 		appObj.onclickInside = execCommand
+		appObj.name = app["title"]  #Debug purpose
 		appObj.data = app
-		screen.addObj(appObj)	
+		appObj.tags.add("page"+str(current_page))
+		print("Creating object : " + str(appObj.name))
+		screen.addObj(appObj)
 	# Page change and reboot button (tools)
 	pagebtn_x=rect_x+2*big_border
 	pagabtn_y=(apps_per_page+2)*rectHeight + int(apps_per_page*rectHeight/3)
@@ -72,15 +71,19 @@ def printLauncher(page=current_page):
 	color = black if current_page>0 else light_gray
 	previousObj = POL.rectangle(pagebtn_x_temp,pagabtn_y,pagebtn_w,pagebtn_h,fill=white,outline=rect_outline)
 	previousObj = POL.add_centeredText(previousObj,text,small_font,color)
+	previousObj.name = "previousPage"
 	previousObj.onclickInside = previousPage
 	previousObj.data = color == black
+	previousObj.tags.add("page"+str(current_page))
 	screen.addObj(previousObj)
 	#Current and reboot
 	pagebtn_x_temp += pagebtn_w + 2*big_border
 	text="Page " + str(current_page) + '\r\n' + "Reboot"
 	currentObj = POL.rectangle(pagebtn_x_temp,pagabtn_y,pagebtn_w,pagebtn_h,fill=white,outline=rect_outline)
 	currentObj = POL.add_centeredText(currentObj,text,small_font,0)
-	previousObj.onclickInside = reboot
+	currentObj.name = "reboot"
+	currentObj.onclickInside = reboot
+	currentObj.tags.add("page"+str(current_page))
 	screen.addObj(currentObj)
 	#Next
 	pagebtn_x_temp += pagebtn_w + 2*big_border
@@ -88,49 +91,71 @@ def printLauncher(page=current_page):
 	color = black if len(appsData["apps"][page*8:])>8 else light_gray
 	nextObj = POL.rectangle(pagebtn_x_temp,pagabtn_y,pagebtn_w,pagebtn_h,fill=white,outline=rect_outline)
 	nextObj = POL.add_centeredText(nextObj,text,small_font,color)
-	previousObj.onclickInside = nextPage
-	previousObj.data = color == black
+	nextObj.name = "nextPage"
+	nextObj.onclickInside = nextPage
+	nextObj.data = color == black
+	nextObj.tags.add("page"+str(current_page))
 	screen.addObj(nextObj)
 
 
-def execCommand(obj):
+def execCommand(objId):
 	print("ExecCommand executed")
 	#Touch indicator
-	obj.invert(1)
+	obj=screen.findObjWithId(objId)
+	screen.printInvertedObj(1,objId)
 	if obj.data["killKPLOnClick"]:
 		#Closing this FBInk session
-		pssm.FBInk.fbink_close(pssm.fbfd)
+		screen.device.closePrintHandler()
 		#Closing touch file
-		touch.close()
+		screen.device.closeInteractionHandler()
 	#executing the actual command
 	os.system(obj.data["command"])
 
-def previousPage(obj):
-	obj.invert(1)
+def previousPage(objId):
+	global current_page
+	# screen.printInvertedObj(1,obj)
+	obj=screen.findObjWithId(objId)
 	if obj.data:
-		printLauncher(current_page-1) 
+		screen.removeAllWithTag("page"+str(current_page))
+		printLauncher(current_page-1)
 
 
-def nextPage(obj):
-	obj.invert(1)
+def nextPage(objId):
+	global current_page
+	# screen.printInvertedObj(1,obj)
+	obj=screen.findObjWithId(objId)
 	if obj.data:
-		printLauncher(current_page+1) 
+		screen.removeAllWithTag("page"+str(current_page))
+		printLauncher(current_page+1)
 
-def reboot(obj):
-	obj.invert(0)
+def reboot(rebootObjId):
+	screen.printInvertedObj(1,rebootObjId)
 	os.system("reboot")
 
 
-screenWidth = pssm.screen_width
-screenHeight = pssm.screen_height
+def debug(objId):
+	for obj in screen.stack:
+		print(obj, obj.name, obj.tags, obj.y, obj.id)
 
-# INITIALIZING TOUCH
-touchPath = "/dev/input/event1"
-touch = KIP.inputObject(touchPath, screenWidth, screenHeight)
 
-screen = pssm.ScreenStackManager(touch,'Main Manager')
+screen = pssm.ScreenStackManager(pssm_kobo,'Main Manager')
+screenWidth = screen.width
+screenHeight = screen.height
 # screen.refresh()
+screen.clear()
 screen.createCanvas()
+screen.refresh()
+# Main title
+rectWidth=int(5*screenWidth/6)
+titleRectHeight=int(screenHeight/10)
+rectHeight = int(screenHeight/(apps_per_page+7))
+rect_x = int(screenWidth/12)
+titleObj = POL.roundedRectangle(rect_x,big_border,rectWidth,titleRectHeight,fill=rect_color,outline=rect_outline)
+titleObj = POL.add_centeredText(titleObj,"Welcome to the PythonLauncher!",title_font_bold)
+titleObj.name = "TITLE"
+titleObj.onclickInside=debug
+screen.addObj(titleObj)
+# Menu entries
 printLauncher(page=0)
 
 # screen.startListenerThread()
