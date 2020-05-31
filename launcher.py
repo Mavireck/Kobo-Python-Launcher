182eb27a91c7b19cdc0a555ed64e8a89ade727ff
Mavireck <mavireck@outlook.fr>
2020-05-31 16:57:08 +0200

    Fixed Launcher

--- launcher.py
+++ launcher.py
@@ -15,11 +15,12 @@ import pssm
 current_page = 0
 apps_per_page=8
 last_page_id= -1
-
+menuLayout = None
 
 def printPage(page=0):
 	global current_page
 	global last_page_id
+	global menuLayout
 	current_page = page
 	number_of_items = min(apps_per_page,len(appsData["apps"][page*apps_per_page:(page+1)*apps_per_page]))
 	list_items = []
@@ -27,8 +28,8 @@ def printPage(page=0):
 		app = appsData["apps"][page*apps_per_page+i]
 		list_items.append({
 			'text' : app["title"],
-			'font_size' : 20,
-			'onclickInside' : execCommand,
+			'font_size' : "h*0.4",
+			'onclickInside' : execAction,
 			'user_data' : app,
 			'invertOnClick': True,
 			'invertDuration': 0
@@ -37,35 +38,62 @@ def printPage(page=0):
 	for i in range(apps_per_page-number_of_items):
 		list_items.append({
 			'text' : "",
-			'font_size' : 20,
 			'invertOnClick': False,
-			'outline_color':pssm.white
+			'outline_color': "white"
 		})
 	buttonList  = pssm.ButtonList(buttons=list_items, margins=[30,30,100,100], spacing=10)
 	# Now for the other buttons
-	button_welcome  = pssm.Button(text="Welcome to the PythonLauncher!",radius=20, background_color = 220, font=pssm.Merri_bold, font_size = 35, wrap_textOverflow = False)
+	button_welcome  = pssm.Button(
+		text = "Welcome to the PythonLauncher!",
+		font = "default-Bold",
+		font_size = "w*0.05",
+		radius = 20,
+		background_color = "gray14",
+		wrap_textOverflow = False
+	)
 	reboot_text     = "Page " + str(current_page) + '\r\n' + "Reboot"
 	is_previousPage = current_page>0
 	is_nextPage 	= len(appsData["apps"][page*8:])>8
-	previous_color  = pssm.black if is_previousPage else pssm.light_gray
-	next_color      = pssm.black if is_nextPage else pssm.light_gray
-	button_reboot   = pssm.Button(reboot_text, onclickInside = reboot, invertOnClick	= True, invertDuration = 0)
-	button_previous = pssm.Button("Previous",  onclickInside = previousPage, text_color = previous_color, user_data = is_previousPage, invertOnClick= True, invertDuration = 0)
-	button_next     = pssm.Button("Next",      onclickInside = nextPage,     text_color = next_color    , user_data = is_nextPage, invertOnClick	= True, invertDuration = 0)
+	previous_color  = "black" if is_previousPage else "gray12"
+	next_color      = "black" if is_nextPage else "gray12"
+	button_reboot   = pssm.Button(
+		reboot_text,
+		font_size = "h*0.25",
+		onclickInside = reboot,
+		invertOnClick	= True,
+		invertDuration = 0
+	)
+	button_previous = pssm.Button(
+		"Previous",
+		font_size = "h*0.25",
+		onclickInside = previousPage,
+		font_color = previous_color,
+		user_data = is_previousPage,
+		invertOnClick= True,
+		invertDuration = 0
+	)
+	button_next     = pssm.Button(
+		"Next",
+		font_size = "h*0.25",
+		onclickInside = nextPage,
+		font_color = next_color    ,
+		user_data = is_nextPage,
+		invertOnClick	= True,
+		invertDuration = 0
+	)
 	menu = [
 		[30                                                                                                             ],
-		[130,            (None,80),                       (button_welcome,"?"),                    (None,80)            ],
+		["H/7",            (None,80),                       (button_welcome,"?"),                    (None,80)            ],
 		["?",                                               (buttonList,"?")                                            ],
-		[100,(None,30), (button_previous,"?"), (None,30), (button_reboot,"?"), (None,30), (button_next,"?"), (None,30)  ],
+		["H/12",(None,30), (button_previous,"?"), (None,30), (button_reboot,"?"), (None,30), (button_next,"?"), (None,30)  ],
 		[30                                                                                                             ]
 	]
 	menuLayout = pssm.Layout(menu,screen.area)
-	last_page_id = menuLayout.id
 	screen.addElt(menuLayout)
 
 
-def execCommand(elt,coords):
-	print("ExecCommand executed")
+def execAction(elt,coords):
+	print("execAction executed")
 	if elt.user_data["killKPLOnClick"]:
 		#Closing this FBInk session
 		screen.device.closePrintHandler()
@@ -78,14 +106,14 @@ def previousPage(elt,coords):
 	global current_page
 	global last_page_id
 	if elt.user_data:
-		screen.removeElt(last_page_id,skipPrint=True)
+		screen.removeElt(menuLayout,skipPrint=True)
 		printPage(current_page-1)
 
 def nextPage(elt,coords):
 	global current_page
 	global last_page_id
 	if elt.user_data:
-		screen.removeElt(last_page_id,skipPrint=True)
+		screen.removeElt(menuLayout,skipPrint=True)
 		printPage(current_page+1)
 
 def reboot(elt,coords):
@@ -94,7 +122,7 @@ def reboot(elt,coords):
 
 ################################################################################
 #Declare the Screen Stack Manager
-screen = pssm.ScreenStackManager(device,'Main')
+screen = pssm.PSSMScreen(device,'Main')
 #Start Touch listener, as a separate thread
 screen.startListenerThread(grabInput=True)
 #Clear and refresh the screen
